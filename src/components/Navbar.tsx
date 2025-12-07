b909c59f5db7e12a018fa0da499b5d4f82867b0b
Rufus Emmanuel <Rufusemmanuel@users.noreply.github.com>
2025-12-07 20:21:08 +0100

    Remove History nav link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,6 @@ import Image from 'next/image';
 
 const navItems = [
   { label: 'Markets', href: '#markets' },
-  { label: 'History', href: '#history' },
   { label: 'About', href: '#about' },
 ];
 
